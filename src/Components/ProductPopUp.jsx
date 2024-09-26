3773854037b1ca3ea65fe5e1af52f33f6eb43fd6
rookierohan10 <115941195+rookierohan10@users.noreply.github.com>
2024-09-26 12:07:05 +0000

    Working on Pop-up

--- src/Components/ProductPopUp.jsx
+++ src/Components/ProductPopUp.jsx
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './css files/ProductPopUp.css'
+import { ChevronLeft, ChevronRight, X } from 'lucide-react'
 
 const ProductPopUp = (props) => {
   const {openModel, modelContent, closeContent} = props
   const {image1, image2} = modelContent.product_image
-  console.log(modelContent)
+  const [image, setImage] = useState(image1);
+
+  useEffect(()=>{
+    setImage(image1);
+  },[image1]);
+
+
+  const handleClick = () =>{
+    console.log('clicked')
+    if(image === image1) {
+      setImage(image2)
+    } else {
+      setImage(image1)
+    }
+  }
+
   return (
     <div className='ProductPopUp'>
+      <div className='cross-mark'><X/></div>
       <div className='imageContainer'>
         <div className='imageHolder'>
-          <img src={image1} alt={modelContent.product_name}/>
+          {image2 && <div className='left-button' onClick={() => handleClick()}> <ChevronLeft color='white'/> </div>}
+          {image2 && <div className='right-button' onClick={() => handleClick()}> <ChevronRight color='white'/> </div>}
+          <img src={image} alt={modelContent.product_name}/>
         </div>
       </div>
-      <div className='infoContainer'>Info Container</div>
+      <div className='infoContainer'>
+        <div className='product-name'></div>
+        <div></div>
+      </div>
     </div>
   )
 }
