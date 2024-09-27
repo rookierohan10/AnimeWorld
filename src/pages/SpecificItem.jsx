d5f5f691dfe2c68d96ad363c645c28d140ab4e8b
rookierohan10 <115941195+rookierohan10@users.noreply.github.com>
2024-09-27 14:36:16 +0000

    Working on product page

--- src/pages/SpecificItem.jsx
+++ src/pages/SpecificItem.jsx
@@ -4,35 +4,38 @@ import './css files/SpecificItem.css'
 import Loader from '../Components/Loader';
 import { useSearchParams } from 'react-router-dom';
 import { supabase } from '../supabase/supabaseClient';
+import { Gift, RotateCw, Truck } from 'lucide-react';
 
 const SpecificItem = () => {
   const [productInfo, setProductInfo] = useState(false);
   const [image, setImage] = useState(null)
+  const [images, setImages] = useState(null)
   const [searchParams] = useSearchParams()
   const id = searchParams.get('id')
-  var {image1, image2} = {image1: null, image2: null}
 
   const fetchSpecific = async () => {
-    const {data, error} = await supabase
+    const { data, error } = await supabase
       .from('Products')
       .select('*')
-      .eq('id',id)
+      .eq('id', id)
       .single();
 
-    if(error) {
+    if (error) {
       console.log('Error loading Product Info')
     } else {
-      setProductInfo(true)
+      setImage(data.product_image.image1)
+      setImages(Object.values(data.product_image))
+      setProductInfo(data)
     }
+  }
+
+  const handleImageChange = (pic) => {
+    setImage(pic)
   }
 
   useEffect(() => {
     fetchSpecific()
-  },[id])
-
-  useEffect(() => {
-    const {image1, image2} = productInfo.product_image
-  },[productInfo])
+  }, [id])
 
   return (
     <section className='Specific-Product-Page'>
@@ -42,10 +45,28 @@ const SpecificItem = () => {
           <>
             <div className='image-and-info'>
               <div className='image-showcase'>
-
+                <div className='image-options'>
+                  {images.map(pic => <div onClick={() => handleImageChange(pic)}><img src={pic} /></div>)}
+                </div>
+                <div className='image-main'>
+                  <img src={image}></img>
+                </div>
               </div>
               <div className='product-showcase'>
-
+                <div className='product-name'>{productInfo.product_name}</div>
+                <div className='price-info'>
+                  <div>
+                    <div className='discounted-price'>{productInfo.discounted_price}</div>
+                    <div className='original-price'>{productInfo.original_price}</div>
+                    <div className='sale-tag'>SALE</div>
+                  </div>
+                  <div className='tax-tag'>Tax Included</div>
+                </div>
+                <div className='add'>
+                  <div className='pointers'><RotateCw />Easy Returns and Exchanges.</div>
+                  <div className='pointers'><Gift />Free Shipping above ₹399.00</div>
+                  <div className='pointers'><Truck />Fast shipping across India.</div>
+                </div>
               </div>
             </div>
             <div className='description'>
