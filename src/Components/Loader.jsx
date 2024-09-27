6c63c85570b41a0e53cfe7de8d1f60db7368ef9c
rookierohan10 <115941195+rookierohan10@users.noreply.github.com>
2024-09-27 12:19:23 +0000

    worked on individual product page

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './css files/Loader.css'
+
+const Loader = () => {
+  return (
+    <div className='loader-background'>
+        <div className='logo'>AW</div>
+    </div>
+  )
+}
+
+export default Loader
--- src/pages/SpecificItem.jsx
+++ src/pages/SpecificItem.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../Components/Navbar'
+import './css files/SpecificItem.css'
+import Loader from '../Components/Loader';
 
 const SpecificItem = () => {
+  const [productInfo, setProductInfo] = useState(false);
+
+  const fetchSpecific = () => {
+    
+  }
+
   return (
     <section className='Specific-Product-Page'>
       <Navbar />
+      <section className='Product-Information'>
+        {productInfo === false ? <Loader /> :
+          <>
+            <div className='image-and-info'>
+              <div className='image-showcase'>
+
+              </div>
+              <div className='product-showcase'>
+
+              </div>
+            </div>
+            <div className='description'>
+
+            </div>
+          </>
+
+        }
+      </section>
     </section>
   )
 }
