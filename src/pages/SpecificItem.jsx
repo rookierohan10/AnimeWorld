b2241b1444fc9fffedfa331dbe88e268acfc0ab2
rookierohan10 <115941195+rookierohan10@users.noreply.github.com>
2024-09-27 13:00:23 +0000

    working on individual product page

--- src/pages/SpecificItem.jsx
+++ src/pages/SpecificItem.jsx
@@ -1,14 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Components/Navbar'
 import './css files/SpecificItem.css'
 import Loader from '../Components/Loader';
+import { useSearchParams } from 'react-router-dom';
+import { supabase } from '../supabase/supabaseClient';
 
 const SpecificItem = () => {
   const [productInfo, setProductInfo] = useState(false);
+  const [image, setImage] = useState(null)
+  const [searchParams] = useSearchParams()
+  const id = searchParams.get('id')
+  var {image1, image2} = {image1: null, image2: null}
 
-  const fetchSpecific = () => {
-    
+  const fetchSpecific = async () => {
+    const {data, error} = await supabase
+      .from('Products')
+      .select('*')
+      .eq('id',id)
+      .single();
+
+    if(error) {
+      console.log('Error loading Product Info')
+    } else {
+      setProductInfo(true)
+    }
   }
+
+  useEffect(() => {
+    fetchSpecific()
+  },[id])
+
+  useEffect(() => {
+    const {image1, image2} = productInfo.product_image
+  },[productInfo])
 
   return (
     <section className='Specific-Product-Page'>
