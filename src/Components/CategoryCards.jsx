c65b62b7f539e7fbf81482df98437ae1b695852d
rookierohan10 <115941195+rookierohan10@users.noreply.github.com>
2024-10-14 13:03:36 +0000

    completed with categories page

--- src/Components/CategoryCards.jsx
+++ src/Components/CategoryCards.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './css files/CategoryCard.css'
+import { Gift } from 'lucide-react';
 
 const CategoryCards = (props) => {
-  const {key, name, image} = props;
+  const { key, name, image } = props;
+  const [category_image, setCategoryImage] = useState(true)
   return (
     <div className='category-card'>
-        <div className='category-image'>
-            <img src={image}></img>
-        </div>
-        <div className='category-name'>{name}</div>
+      <div className='category-image'>
+        {image ?
+          <img
+            src={image}
+            onError={() => setCategoryImage(false)}
+          /> :
+          <Gift />
+        }
+      </div>
+      <div className='category-name'>{name}</div>
     </div>
   )
 }
--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from "../Components/Navbar";
+import Loader from "../Components/Loader"
 import './css files/Collections.css'
 import { supabase } from "../supabase/supabaseClient";
+import CategoryCards from '../Components/CategoryCards';
 
 
 const Collections = () => {
 
     const [categories, setCategories] = useState([])
+    const [loading, isLoading] = useState(false)
 
     const fetchCategories = async () => {
         const { data, error } = await supabase
@@ -16,13 +19,12 @@ const Collections = () => {
             console.log('Error in loading categories')
             console.log('Error')
         } else {
-            const transformed_data = data.map(({ category_name, category_image }) => ({
+            return data.map(({ category_name, category_image }) => ({
                 id: category_name,
                 name: category_name,
                 image: category_image,
                 tag: 'category'
             }))
-            setCategories((prevCategories) => [...prevCategories, transformed_data])
         }
     }
 
@@ -34,27 +36,52 @@ const Collections = () => {
             console.log('Error in loading categories')
             console.log('Error')
         } else {
-            const transformed_data = data.map(({ anime_id, anime_name, anime_image }) => ({
+            return data.map(({ anime_id, anime_name, anime_image }) => ({
                 id: anime_id,
                 name: anime_name,
                 image: anime_image,
                 tag: 'category'
             }))
-            setCategories((prevCategories) => [...prevCategories, transformed_data])
         }
     }
 
     useEffect(() => {
-        fetchCategories()
-        fetchAnime()
-    })
+        const fetchData = async () => {
+            const categories = await fetchCategories()
+            const anime = await fetchAnime()
 
-    useEffect(()=>{
+            setCategories([...categories, ...anime])
+        }
+
+        fetchData()
+    }, [])
+
+    useEffect(() => {
         console.log(categories)
-    },[categories])
+    }, [categories])
     return (
         <section className="collections-section">
             <Navbar></Navbar>
+            <div className='categories-div'>
+                {!isLoading ?
+                    <div className='loader-div'>
+                        <Loader></Loader>
+                    </div>
+                    :
+                    <div className='categories-container'>
+                        <div className='categories-header'>Categories</div>
+                        <div className='categories'>
+                            {categories.map(category => 
+                                <CategoryCards
+                                    key={category.id}
+                                    id={category.id}
+                                    name={category.name}
+                                    image={category.image}
+                                />
+                            )}
+                        </div>
+                    </div>}
+            </div>
         </section>
     )
 }
