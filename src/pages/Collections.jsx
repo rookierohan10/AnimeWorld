531930ee50dc125d6b93b5ee9efd1fb7a7f9b34c
rookierohan10 <115941195+rookierohan10@users.noreply.github.com>
2024-10-14 08:55:13 +0000

    working on categories page

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
 import Navbar from "../Components/Navbar";
 import './css files/Collections.css'
 import { supabase } from "../supabase/supabaseClient";
 
-const fetchCategories = async () => {
-    const { data, error } = await supabase
-      .from('Category').select().order('category_name').limit(10);
-}
 
 const Collections = () => {
+
+    const [categories, setCategories] = useState([])
+
+    const fetchCategories = async () => {
+        const { data, error } = await supabase
+            .from('Category').select('*');
+
+        if (error) {
+            console.log('Error in loading categories')
+            console.log('Error')
+        } else {
+            const transformed_data = data.map(({ category_name, category_image }) => ({
+                id: category_name,
+                name: category_name,
+                image: category_image,
+                tag: 'category'
+            }))
+            setCategories((prevCategories) => [...prevCategories, transformed_data])
+        }
+    }
+
+    const fetchAnime = async () => {
+        const { data, error } = await supabase
+            .from('Anime').select('*');
+
+        if (error) {
+            console.log('Error in loading categories')
+            console.log('Error')
+        } else {
+            const transformed_data = data.map(({ anime_id, anime_name, anime_image }) => ({
+                id: anime_id,
+                name: anime_name,
+                image: anime_image,
+                tag: 'category'
+            }))
+            setCategories((prevCategories) => [...prevCategories, transformed_data])
+        }
+    }
+
+    useEffect(() => {
+        fetchCategories()
+        fetchAnime()
+    })
+
+    useEffect(()=>{
+        console.log(categories)
+    },[categories])
     return (
         <section className="collections-section">
             <Navbar></Navbar>
